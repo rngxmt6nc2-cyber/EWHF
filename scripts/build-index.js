e42a997337f2619f279542ff33323423ef6b276a
rngxmt6nc2-cyber <rngxmt6nc2@privaterelay.appleid.com>
2026-03-23 13:41:34 -0400

    Update build-index.js

--- scripts/build-index.js
+++ scripts/build-index.js
@@ -1,1 +1,18 @@
 
+const fs = require("fs");
+const path = require("path");
+
+const recipesDir = path.join(__dirname, "..", "recipes");
+const outputFile = path.join(__dirname, "..", "index.json");
+
+const files = fs
+  .readdirSync(recipesDir)
+  .filter((file) => file.endsWith(".json"))
+  .sort();
+
+const data = {
+  recipes: files.map((file) => path.basename(file, ".json"))
+};
+
+fs.writeFileSync(outputFile, JSON.stringify(data, null, 2) + "\n");
+console.log(`Built index.json with ${data.recipes.length} recipes`);
